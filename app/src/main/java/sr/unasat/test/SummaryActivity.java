94527e0d7e5e220a15d11e860f97fc09e14920ae
JuscelinoKartoebi <jkartoebi@gmail.com>
2021-03-30 20:57:56 -0300

    bijna af

--- app/src/main/java/sr/unasat/test/SummaryActivity.java
+++ app/src/main/java/sr/unasat/test/SummaryActivity.java
@@ -3,6 +3,7 @@ package sr.unasat.test;
 
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,11 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -90,4 +96,27 @@ public class SummaryActivity extends AppCompatActivity implements LoaderManager.
 
         mAdapter.swapCursor(null);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.home) {
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
